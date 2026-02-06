e89f5c2b29d882ec214f5e0f6bea6adee3bb0468
Iminokhun <boss151911@gmail.com>
2026-02-06 15:01:36 +0500

    Add attendance system base

---
+++ app/Filament/Resources/Visits/Pages/ListVisits.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Visits\Pages;
+
+use App\Filament\Resources\Visits\VisitResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListVisits extends ListRecords
+{
+    protected static string $resource = VisitResource::class;
+}
+
